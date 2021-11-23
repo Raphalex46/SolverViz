2ab8bc3238521e183904a11f6bdc313dd6ea37c7
Raphaël Colin <raphael.colin68@gmail.com>
2021-11-23 17:26:29 +0100

    Modify geogebra exporter to use xml_write predicate.

--- src/translators/geogebra/geogebra.pl
+++ src/translators/geogebra/geogebra.pl
@@ -4,23 +4,20 @@
 % export(+Translation)
 % Will export the translation to a readable geogebra file.
 export(Translation) :-
-  format(
-    string(FinalTranslation),
-    '<geogebra format="4.0">~n\c
-     <construction>~n\c
-     ~w~n\c
-     </construction>~n\c
-     </geogebra>~n\c
-    ',
-    [Translation]
-  ),
+  FinalTranslation = element(
+    geogebra, [format="4.0"], [
+      element(
+        construction, [], Translation
+      )
+      ]
+    ),
   % Fetch output filename
   load:option(output(Filename)),
   % Add the GGB extension to the final file
   atom_concat(Filename, '.ggb', FilenameGGB),
   % Open and write to the XML file
   open('geogebra.xml', write, Stream),
-  write(Stream, FinalTranslation),
+  xml_write(Stream, FinalTranslation, []),
   close(Stream),
   % Finally, zip the XML to get the final file readable by Geogebra
   fork(PidZIP),
@@ -42,7 +39,7 @@ export(Translation) :-
 
 
 
-translate([], "").
+translate([], []).
 translate([AffHead | AffTail], Translation) :-
   % Deconstruct the affectation
   AffHead = element(ConsName, [name=Output], Input),
@@ -59,7 +56,7 @@ translate([AffHead | AffTail], Translation) :-
   ),
   % Recursive call
   translate(AffTail, TranslationTail),
-  string_concat(TranslationHead, TranslationTail, Translation).
+  Translation = [TranslationHead | TranslationTail].
 
 % export_element(+ElementType, +Coords, +ElementName, -Translation)
 % Helper predicate to export an element (mostly points)
@@ -67,47 +64,36 @@ export_element(ElementType, [X, Y], ElementName, Translation) :-
   % Deconstruct the element
   X = element(literal, [value=XVal], []),
   Y = element(literal, [value=YVal], []),
-  format(
-    string(Translation),
-    '<element type="~w" label="~w">~n\c
-    <show object="true" label="true"/>~n\c
-    <coords x="~w" y="~w" z="1"/>~n\c
-    </element>~n',
-    [ElementType, ElementName, XVal, YVal]
-  ).
-
+  Translation =
+    element(element, [type=ElementType, label=ElementName],
+            [
+              element(show, [object=true, label=true], []),
+              element(coords, [x=XVal, y=YVal, z=1], [])
+            ]).
 
 % export_command(+CommandName, +Input, +Output, -Translation)
 % Helper predicate to export a single command
 export_command(CommandName, Input, Output, Translation) :-
   length(Input, InputLength),
   export_input(Input, InputLength, InputTranslation),
-  format(
-    string(Translation),
-    '<command name="~w">~n\c
-    <input~n\c
-    ~w\c
-    />~n\c
-    <output a0="~w"/>~n\c
-    </command>~n',
-    [CommandName, InputTranslation, Output]
-  ).
+  Translation =
+    element(command, [name=CommandName], 
+            [
+             element(input, InputTranslation, []),
+             element(output, [a0=Output], [])
+            ]).
 
 % export_input(+Input, +InputLength, -Translation)
 % Helper predicate to export an input
-export_input([], _, "").
+export_input([], _, []).
 export_input([InputHead | InputTail], InputLength, Translation) :-
   % Deconstruct the input
   InputHead = element(_, [value=InputValue], []),
   length(InputTail, RemainingLength),
   Index is InputLength - RemainingLength - 1,
-  format(
-    string(TranslationHead),
-    'a~w="~w"~n',
-    [Index, InputValue]
-  ),
+  atom_concat(a, Index, Arg),
   export_input(InputTail, InputLength, TranslationTail),
-  string_concat(TranslationHead, TranslationTail, Translation).
+  Translation = [Arg=InputValue | TranslationTail].
 
 % translate_command_name(+ConstructorName, -GeogebraName)
 % Predicate to translate a SolverViz constructor name to
